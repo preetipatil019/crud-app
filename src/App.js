3dc6f6955da4acc41b5517dff7837b5c9bc36f28
preeti <preetipatil019@gmail.com>
2023-06-02 14:29:32 -0700

    CRUD initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([])
+  const [newItem, setNewItem] = useState('')
+  useEffect(() => {
+    fetchData();
+  }, [])
+  
+  const fetchData = async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/')
+    console.log(response.data)
+    setData(response.data)
+  }
+
+  const deleteItem = async (itemId) => {
+    console.log(itemId)
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${itemId}`)
+    fetchData(); 
+  }
+
+  const addItem = async () => {
+    await axios.post('https://jsonplaceholder.typicode.com/todos/', { name: newItem })
+    fetchData();
+    setNewItem('')
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <h1>CRUD APP</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            {item.title}
+            <button onClick={()=>deleteItem(item.id)}>Delete</button>
+        </li>
+        
+        ))}
+        <li>
+          {newItem}
+        </li>
+      </ul>
+      <input
+        type="text"
+        value={newItem}
+        onChange={(e)=>setNewItem(e.target.value)}
+      />
+      <button onClick={addItem}>ADD ITEM</button>
     </div>
   );
 }
